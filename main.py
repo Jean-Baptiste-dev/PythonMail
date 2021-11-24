35dd130f6b4ad3fe63d5c53671bb4799fcc6fd0a
KOUGBENOU Jean-Baptiste <70972589+Jean-Baptiste-dev@users.noreply.github.com>
2021-11-24 17:26:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import smtplib
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-server.login('jeankougbenou@gmail.com', 'jeanjean')
+server.login('votre_adresse_mail@gmail.com', 'Votre_mot_de_passe')
 
 def Continue():
     print("Voulez-vous envoyer un autre mail ? Oui ou Non")

149d7387d9726bd63435d977285b4904e3a842e1
KOUGBENOU Jean-Baptiste <70972589+Jean-Baptiste-dev@users.noreply.github.com>
2021-11-25 16:56:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import smtplib
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-server.login('votre_adresse_mail@gmail.com', 'Votre_mot_de_passe')
-
+server.login('votre_adresse_mail_gmail', 'votre_mot_de_passe')
+from email.message import EmailMessage
 def Continue():
     print("Voulez-vous envoyer un autre mail ? Oui ou Non")
     reponse: str = input()
@@ -13,18 +13,23 @@ def Continue():
         exit()
 
 def mailtosend():
+    email = EmailMessage()
     email_sender = input("Entrez le mail d'envoi s'il vous plaît: ")
+    email['From'] = email_sender
     print("vous avez entrer : " + email_sender)
     email_receiver = input("Entrez le mail de recuperation s'il vous plaît : ")
+    email['To'] = email_receiver
     print("Vous venez d'entrer : " + email_receiver)
     objet = input("Entrez l'objet du mail s'il vous plaît : ")
+    email['Subject'] = objet
     print("Vous venez d'entrer : " + objet)
     content = input("Entrez votre contenu s'il vous plait: ")
+    email.set_content(content)
     print("Voici votre message : " + content)
     print("Voulez-vous envoyer le mail ? Y si Oui et X si Non")
     envoi: str = input()
     if envoi == 'Y':
-        server.sendmail(email_sender, email_receiver, content)
+        server.send_message(email)
         Continue()
     elif envoi == 'X':
         print("Vous avez refusé l'envoi du mail")
